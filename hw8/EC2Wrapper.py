d910c365f6314b78f1420496bbc7db61ba3cba1a
Samuel Marks <samuelmarks@gmail.com>
2014-11-01 18:47:35 +1100

    Fixed up EC2Wrapper to have some functionality, added timeout threshold handling to start instances in example file

--- hw8/EC2Wrapper.py
+++ hw8/EC2Wrapper.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
-# TODO: Until I can work out how to search for an AMI by name, this module is pretty much useless
-
 from os.path import expanduser, join as path_join
+from sys import stderr
 from random import randint
 from pprint import PrettyPrinter
 
@@ -20,6 +19,7 @@ class EC2Wrapper(object):
                 for i in tuple(e.rstrip('\n').split('=') for e in
                                open(path_join(expanduser('~'), '.aws', 'credentials')).readlines()[1:])}
     instance = None
+    image_id = None
 
     def __init__(self, ami_image_id, random_rename=False, persist=False):
         self.instance_name = ami_image_id
@@ -29,6 +29,10 @@ class EC2Wrapper(object):
         self.persist = persist
 
     def __enter__(self):
+        if self.instance:
+            self.start_instance()
+        else:
+            print >> stderr, 'Warning: No instance instantiated'
         return self
 
     def __exit__(self, ext, exv, trb):
@@ -36,51 +40,64 @@ class EC2Wrapper(object):
             self.delete()
 
     def configure(self, region='ap-southeast-2'):
-        self.conn = connect_to_region(**dict(region_name=region, **self.aws_keys))
+        self.conn = connect_to_region(region_name=region, **self.aws_keys)
         return self.conn  # Hmm, if only I could inherit a class from this connection
 
     def list_all_images(self, filters=None):
         if not filters:  # Not as default arguments, as they should be immutable
             filters = {
                 'architecture': 'x86_64'
-
+                # TODO: Figure out how to search for an AMI by name
             }
         return self.conn.get_all_images(owners=['amazon'], filters=filters)
 
-    def create_instance(self, visibility_timeout=20):
-        self.instance = self.conn.create_instance(self.instance_name, visibility_timeout)
-        return True
+    def create_image_from_instance(self, name, description=None, instance_id=None):
+        if not instance_id:
+            instance_id = self.instance.id
+        self.image_id = self.conn.create_image(instance_id, name, description)
+        return self.image_id
 
     def get_instances(self):
         return self.conn.get_all_instances(filters={'architecture': 'x86_64'})
 
-    def get_instance(self):
-        self.instance = self.conn.get_instance(self.instance_name)
+    def set_instance(self, instance):  # Eww, Java-style method
+        self.instance = instance
         return self.instance
 
-    def get_instance_attributes(self, attribute='All'):
-        return self.conn.get_instance_attributes(instance=self.instance, attribute=attribute)
+    def start_instance(self, instance_id=None):
+        if not instance_id:
+            instance_id = self.instance.id
+        self.conn.start_instances(instance_id)
+        self.instance = self.conn.start_instances(instance_id)[0]
+        return self.instance
 
     def delete(self):
         if not self.instance:
             print "No instance to remove"
             return []
-        return self.conn.delete_instance(self.instance)
+        return self.conn.stop_instances(self.instance.id)  # Stop, don't delete (for now)
 
     @staticmethod
     def run(inst, commands, username='ubuntu'):
         """
-        Returns a tuple of tuples consisting of:
+        Uses boto to figure out how to SSH in and run commands
+
+        :return a tuple of tuples consisting of:
         #    The integer status of the command
         #    A string containing the output of the command
         #    A string containing the stderr output of the command
         """
         ssh_client = sshclient_from_instance(inst, user_name=username,
-                                             ssh_key_file=expanduser(path_join(expanduser('~'),
-                                                                               '.ssh', 'aws', 'private',
-                                                                               'cscie90.pem')))
+                                             ssh_key_file=path_join(expanduser('~'), '.ssh', 'aws',
+                                                                    'private', 'cscie90.pem'))
         return tuple(ssh_client.run(command) for command in commands)
 
     @staticmethod
     def run2(commands, host):
+        """
+        Uses Fabric to execute commands over SSH
+
+        :param commands: callable with one or more usages of run/sudo/cd
+        :param host: DNS name or IP address
+        """
         return execute(commands, host=host)
--- hw8/ec2_and_deploy_example.py
+++ hw8/ec2_and_deploy_example.py
@@ -1,5 +1,7 @@
 from os.path import expanduser, join as path_join
 from functools import partial
+from time import sleep
+from exceptions import BaseException
 
 from fabric.context_managers import cd
 from fabric.contrib.files import exists
@@ -11,6 +13,10 @@ fabric_env.skip_bad_hosts = True
 fabric_env.user = 'ubuntu'
 fabric_env.sudo_user = 'ubuntu'
 fabric_env.key_filename = expanduser(path_join(expanduser('~'), '.ssh', 'aws', 'private', 'cscie90.pem'))
+
+
+class TimeoutError(BaseException):
+    pass
 
 
 def first_run():
@@ -25,18 +31,22 @@ def first_run():
 
 
 def deploy():
-    if not exists('"$HOME"/cscie90-hw8', use_sudo=True):
+    if not exists('"$HOME/cscie90-hw8"', use_sudo=True):
         return first_run()
     with cd('"$HOME/cscie90-hw8"'):
         sudo('git pull', user='ubuntu')
 
 
 def serve():
-    if not exists('"$HOME"/cscie90-hw8'):
-        raise OSError("Folder: '\"$HOME\"/cscie90-hw8' doesn't exists but should")
+    if not exists('"$HOME/cscie90-hw8"'):
+        raise OSError('Folder: "$HOME/cscie90-hw8" doesn\'t exists but should')
+
+    sudo('pkill python || true')
+    # ^ Obviously in production I would use a proper daemon-manager that would handle:
+    # PIDs, backgrounding, logging and respawning
+
     with cd('"$HOME/cscie90-hw8/hw8"'):
-        sudo('pkill python && python server.py', user='ubuntu')
-        # Obviously in production I would use a proper daemon that would handle PIDs and respawning
+        sudo('python server.py &', user='ubuntu')
 
 
 if __name__ == '__main__':
@@ -45,7 +55,18 @@ if __name__ == '__main__':
         hw8_instances = tuple(inst for res in ec2.get_instances() for inst in res.instances
                               if inst.tags.get('Name', '').startswith('hw8'))
         for instance in hw8_instances:
-            run3 = partial(ec2.run2, host=getattr(instance, 'public_dns_name'))
+            ec2.set_instance(instance)
+            tried = 0
+            print 'instance.state =', instance.state
+            while instance.state != 'running':
+                if tried == 0:
+                    ec2.start_instance()
+                    print 'Starting instance and waiting (up to) 2 minutes for it to come up'
+                elif tried > 60:
+                    raise TimeoutError
+                tried += 1
+                sleep(2)
+            run3 = partial(ec2.run2, host=instance.public_dns_name)
             # print run3(lambda: sudo('whoami'))
             print run3(deploy)
             print run3(serve)
